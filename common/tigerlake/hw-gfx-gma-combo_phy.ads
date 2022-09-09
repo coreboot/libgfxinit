d1e74b4d7b941db23d484955231f1b4049ed14f6
Tim Wawrzynczak <twawrzynczak@chromium.org>
2022-09-09 10:53:27 -0600

    gma tgl: Add combo PHY programming sequence
    This is required before any combo PHY ports can be used,  so it will be
    performed early in the modeset.
    
    Squashed clean-up:
    
      gma tgl: Refactor code for enhanced modularity
    
      This patch corrects code indentation and incorporates an internal
      procedure Propagate_To_Group for enhanced modularity. There are no
      changes to functionality.
    
      Signed-off-by: Dinesh Gehlot <digehlot@google.com>
    
    Change-Id: Id0c2495872250e84057c3130cab19dea05a00b75
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-on: https://review.sourcearcade.org/c/libgfxinit/+/459
    Tested-by: Nico Huber <nico.h@gmx.de>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

---
+++ common/tigerlake/hw-gfx-gma-combo_phy.ads
@@ -0,0 +1,20 @@
+--
+-- Copyright (C) 2022 Google, LLC
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+
+private package HW.GFX.GMA.Combo_Phy is
+
+   procedure Initialize;
+   procedure All_Off;
+
+end HW.GFX.GMA.Combo_Phy;
